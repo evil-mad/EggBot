7e0ec5298924dc076747bee51bb112c3d56278ed
Windell Oskay <windell@oskay.net>
2019-09-29 19:22:22 -0700

    Updates for v1 compatibility

--- inkscape_driver/eggbot_presethatch.py
+++ inkscape_driver/eggbot_presethatch.py
@@ -17,8 +17,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
-import inkex
 
+from lxml import etree
+
+from plot_utils_import import from_dependency_import # plotink
+inkex = from_dependency_import('ink_extensions.inkex')
 
 class PresetHatch(inkex.Effect):
     def __init__(self):
