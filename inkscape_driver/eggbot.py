0691bbe8dc9f135703b2006c358284974674093a
Jonas Bähr <jonas.baehr@web.de>
2019-11-30 17:25:56 +0100

    Fix py3 str/bytes issue
    by using `isdigit` as an instance method, it works no matter whether we
    have a py2 "unicode" string or a regular py2/py3 string. Thus we don't
    need to `encode` to ensure a py2 string, which breaks py3 code: encode
    returns a bytes object there.

--- inkscape_driver/eggbot.py
+++ inkscape_driver/eggbot.py
@@ -906,7 +906,7 @@ class EggBot(inkex.Effect):
 
         temp_num_string = 'x'
         string_pos = 1
-        current_layer_name = str_layer_name.encode('ascii', 'ignore').lstrip()  # remove leading whitespace
+        current_layer_name = str_layer_name.lstrip()  # remove leading whitespace
 
         # Look at layer name.  Sample first character, then first two, and
         # so on, until the string ends or the string no longer consists of
@@ -915,14 +915,14 @@ class EggBot(inkex.Effect):
         max_length = len(current_layer_name)
         if max_length > 0:
             while string_pos <= max_length:
-                if str.isdigit(current_layer_name[:string_pos]):
+                if current_layer_name[:string_pos].isdigit():
                     temp_num_string = current_layer_name[:string_pos]  # Store longest numeric string so far
                     string_pos += 1
                 else:
                     break
 
         self.plotCurrentLayer = False  # Temporarily assume that we aren't plotting the layer
-        if str.isdigit(temp_num_string):
+        if temp_num_string.isdigit():
             if self.svgLayer == int(float(temp_num_string)):
                 self.plotCurrentLayer = True  # We get to plot the layer!
                 self.LayersPlotted += 1
